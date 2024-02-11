e62fbf3cc0743fb250ff16d00ffb6bcf1c17b7a0
Doris <sayitloud15@gmail.com>
2024-02-11 18:18:20 +0100

    adding geolocation api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 import { unixFormattedTime } from "./unix.js";
 
-let weatherData;
-
 const form = document.querySelector('.weather__form');
 form.addEventListener('submit', getLatLon)
 
@@ -25,18 +23,26 @@ async function getWeatherData(lat, lon) {
 
     listWeatherData(weatherData);
 
-    }
+};
+
+function currentWeather(){
+    navigator.geolocation.getCurrentPosition((currentCity) => {
+        getWeatherData(currentCity.coords.latitude, currentCity.coords.longitude);
+    });
+};
+
+currentWeather();
  
 function listWeatherData(weatherData){
     const list = document.querySelector('.weather__list');
 
     list.innerHTML = `
-        <li class="weather__list-item-icon"><img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png" alt=""></li> 
-        <li class="weather__list-item"><span>Wetter:</span><span>${weatherData.weather[0].description}</span></li>
-        <li class="weather__list-item"><span>Teparatur:</span><span>${Math.round(weatherData.main.temp)} °C</span></li>
-        <li class="weather__list-item"><span>Fühlt sich an wie:</span><span>${Math.round(weatherData.main.feels_like)} °C</span></li>
-        <li class="weather__list-item"><span>Sonnenaufgang:</span><span>${unixFormattedTime(weatherData.sys.sunrise)} Uhr</span></li>
-        <li class="weather__list-item"><span>Sonnenuntergang:</span><span>${unixFormattedTime(weatherData.sys.sunset)} Uhr</span></li>
+        <li class="weather__list-item weather__list-item-icon"><img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png" alt=""></li> 
+        <li class="weather__list-item weather__list-item-info"><span>${weatherData.weather[0].description}</span></li>
+        <li class="weather__list-item"><span>Teparatur</span><span>${Math.round(weatherData.main.temp)} °C</span></li>
+        <li class="weather__list-item"><span>Fühlt sich an wie</span><span>${Math.round(weatherData.main.feels_like)} °C</span></li>
+        <li class="weather__list-item"><span>Sonnenaufgang</span><span>${unixFormattedTime(weatherData.sys.sunrise)} Uhr</span></li>
+        <li class="weather__list-item"><span>Sonnenuntergang</span><span>${unixFormattedTime(weatherData.sys.sunset)} Uhr</span></li>
     `;
 };
 
